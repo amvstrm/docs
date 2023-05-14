13179aa1b17bb462ec2a5143570d408eddcfd260
nyt92 <chamroeunlinda12343@gmail.com>
2023-05-14 17:51:56 +0700

    upload all the code

---
+++ app.config.ts
@@ -0,0 +1,27 @@
+export default defineAppConfig({
+  docus: {
+    title: 'amvstrm',
+    description: 'amvstrm is an open source anime streaming platform that offer free anime with no ADs.',
+    image: 'https://raw.githubusercontent.com/amvstrm/amvstrm/v2/public/amv_wh.png',
+    socials: {
+      github: 'amvstrm/docs'
+    },
+    aside: {
+      level: 0,
+      exclude: []
+    },
+    header: {
+      title: 'Logo',
+      logo: true,
+    },
+    footer: {
+      iconLinks: [
+        {
+          href: 'https://amvstr.ml',
+          icon: '',
+          label: 'amvstr.ml'
+        }
+      ]
+    }
+  }
+})
